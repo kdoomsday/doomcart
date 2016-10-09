932ede1a3ca313bc2610d4cac3c14693c255c01a
Eduardo Barrientos <kdoomsday@gmail.com>
2016-10-08 22:27:55 -0400

    All normal actions for login should be in webCore. This means no routing through HomeChooser, creating main.scala and a home page in the module, etc.

--- modules/webCore/app/webCore/controllers/homeChooser/HomeChooser.scala
+++
@@ -1,7 +0,0 @@
-package webCore.controllers.homeChooser
-
-/** Injectable home chooser so that implementations can decide where home is */
-trait HomeChooser {
-  /** URI to teh home page */
-  def home: String
-}
