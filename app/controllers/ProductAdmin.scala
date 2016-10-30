b22ec25f8386db731c34aad656153b24e7d49d91
Eduardo Barrientos <kdoomsday@gmail.com>
2016-10-29 22:16:18 -0400

    Add product images
    This has both adding a new product with an image and adding images to a
    product. This last one, however, is untested.

--- app/controllers/ProductAdmin.scala
+++ app/controllers/ProductAdmin.scala
@@ -4,13 +4,13 @@ import daos.ImgSave
 import models.Notification
 import play.api.libs.Files.TemporaryFile
 import play.api.mvc.{ Controller, MultipartFormData => MPFD }
+import MPFD.{ FilePart => FilePart }
 import play.api.data.Forms._
 import play.api.data._
 import play.api.i18n.{ I18nSupport, MessagesApi }
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import javax.inject.Inject
-import MPFD.{ FilePart => FilePart }
 
 import actions.Actions
 import daos.ProductDao
@@ -46,13 +46,31 @@ class ProductAdmin @Inject() (
       }
     )
   }
+
+  def addImageView(productId: Long) = actions.roleAction("employee") { implicit req =>
+    Future.successful( Ok( views.html.addImageView(productId) ) )
+  }
+
+  def addImage(productId: Long) = actions.roleActionP("employee")(parse.multipartFormData) { implicit req =>
+    req.body.file("image").fold {
+      implicit val nots = Seq(Notification("warn", messagesApi("ProductAdmin.addImage.noImage")))
+      Future.successful( Redirect( routes.Application.employeeIndex() ) )
+    }{
+      fpart => {
+        productDao.addImage(productId, fpart).map {_ =>
+          implicit val nots = Seq(Notification("success", messagesApi("ProductAdmin.addImage.success")))
+          Redirect(routes.Application.employeeIndex())
+        }
+      }
+    }
+  }
 }
 
 object ProductAdmin {
   import daos.ProductDao.ProductInfo
 
   lazy val productForm = Form(
-    mapping(
+    mapping (
       "name"  -> nonEmptyText,
       "price" -> bigDecimal(17, 2)
     )(ProductInfo.apply)(ProductInfo.unapply)
